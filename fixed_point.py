36e05dc2b35535ebc55f7591042b4393c96c0a4d
Amitabha Roy <amitabha.roy@gmail.com>
2023-08-10 21:33:22 +0530

    Update fixed_point.py
    Add gradient computations.

--- fixed_point.py
+++ fixed_point.py
@@ -11,6 +11,8 @@ import time
 
 _LOOKBACK = 5
 _VECTOR_DIM = 1000
+_MAX_ITER = 1000
+_TOL = 1e-5
 
 
 @jit
@@ -24,52 +26,57 @@ def anderson_update(G_lookback, X_lookback, z_new):
     return z_new
 
 
-class AndersonFixedPoint:
-
-    def __init__(self, tol, max_iter, features):
-        self.tol = tol
-        key = random.PRNGKey(0)
-        # N(0, 0.01) - important for contractive mapping.
-        # sd should be set per vector dimension.
-        self.projection = random.normal(
-            key, shape=(features, features))/100
-        self.max_iter = max_iter
-        self.iterations = 0
-
-    def linear(self, z, x):
-        return self.projection@z + x
-
-    def step(self, z, x):
-        return jnp.tanh(self.linear(z, x))
-
-    def compute_fp(self, x):
-        z = jnp.tanh(x)
-        X_lookback = jnp.zeros(shape=(_LOOKBACK, z.shape[0]))
-        G_lookback = jnp.zeros(shape=(_LOOKBACK, z.shape[0]))
-        start = time.time()
-        for iter in range(self.max_iter):
-            self.iterations += 1
-            z_new = self.step(z, x)
-            if jnp.linalg.norm(z - z_new) < self.tol:
-                break
-            X_lookback = jnp.roll(X_lookback, shift=1, axis=0)
-            X_lookback = X_lookback.at[0].set(z)
-            G_lookback = jnp.roll(G_lookback, shift=1, axis=0)
-            G_lookback = G_lookback.at[0].set(z_new - z)
-            if self.iterations >= _LOOKBACK:
-                z_new = anderson_update(
-                    G_lookback, X_lookback, z_new)
-            z = z_new
-        end = time.time()
-        print(f'FP Iters = {self.iterations} at '
-              f'{(end - start)/self.iterations} sec/iter')
-        return z
+key = random.PRNGKey(0)
+# N(0, 0.01) - important for contractive mapping.
+# sd should be set per vector dimension.
+projection = random.normal(key, shape=(_VECTOR_DIM, _VECTOR_DIM))/100
 
 
-fp_solver = AndersonFixedPoint(1e-5, 100, _VECTOR_DIM)
+def linear(z, x):
+    return projection@z + x
+
+
+def step(z, x):
+    return jnp.tanh(linear(z, x))
+
+
+def compute_fp(x):
+    z = jnp.tanh(x)
+    X_lookback = jnp.zeros(shape=(_LOOKBACK, z.shape[0]))
+    G_lookback = jnp.zeros(shape=(_LOOKBACK, z.shape[0]))
+    start = time.time()
+    for iter in range(1, 1 + _MAX_ITER):
+        z_new = step(z, x)
+        if jnp.linalg.norm(z - z_new) < _TOL:
+            break
+        X_lookback = jnp.roll(X_lookback, shift=1, axis=0)
+        X_lookback = X_lookback.at[0].set(z)
+        G_lookback = jnp.roll(G_lookback, shift=1, axis=0)
+        G_lookback = G_lookback.at[0].set(z_new - z)
+        if iter >= _LOOKBACK:
+            z_new = anderson_update(
+                G_lookback, X_lookback, z_new)
+        z = z_new
+    end = time.time()
+    print(f'FP Iters = {iter} at '
+          f'{(end - start)/iter} sec/iter')
+    return z
+
+
+def compute_gradient(z, x):
+    ''' Use implicit function theorem to compute gradient.'''
+    delf_x = jnp.eye(z.shape[0])
+    diag_indices = jnp.diag_indices_from(delf_x)
+    delf_x = delf_x.at[diag_indices].set(1.0/jnp.cosh(linear(z, x))**2)
+    delf_z = delf_x@projection
+    return jax.scipy.linalg.solve(jnp.eye(z.shape[0]) - delf_z, delf_x)
+
+
 key = random.PRNGKey(100)
 x = random.normal(key, shape=(_VECTOR_DIM,))
 print(f'Device = {x.device()}')
-fixed_point = fp_solver.compute_fp(x)
-jacobian = jax.jacfwd(fp_solver.compute_fp)
-print('tol = ', jnp.linalg.norm(fixed_point - fp_solver.step(fixed_point, x)))
+fixed_point = compute_fp(x)
+jacobian = jax.jacfwd(compute_fp)(x)
+print('tol = ', jnp.linalg.norm(fixed_point - step(fixed_point, x)))
+implicit_fn_jacobian = compute_gradient(fixed_point, x)
+print('gradient err = ', jnp.linalg.norm(jacobian - implicit_fn_jacobian))
