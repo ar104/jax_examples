d0fb579442de1d27e22af2e5aa5c669754331322
Amitabha Roy <amitabha.roy@gmail.com>
2023-08-25 15:14:24 +0530

    Update hm_post.py
    Vectorize for speed.

--- hm/hm_post.py
+++ hm/hm_post.py
@@ -1,7 +1,7 @@
 from collections import defaultdict
 import pandas as pd
+import jax
 import jax.numpy as jnp
-from jax import jit
 from tqdm import tqdm
 import time
 
@@ -9,14 +9,57 @@ _DATASET = 'C:\\Users\\aroym\\Downloads\\hm_data'
 _EMBEDDINGS = 'C:\\Users\\aroym\\OneDrive\\Documents\\GitHub\\jax_examples\\hm\\embeddings_23.npz'
 _HISTORY = 64
 _K = 12
+_BATCH = 4096
 
 
-@jit
+@jax.jit
 def get_topk(history, embeddings):
     user_embedding = jnp.mean(history, axis=0)
     scores = embeddings @ jnp.transpose(user_embedding)
     topk = jnp.argsort(-scores)[:_K]
     return topk
+
+
+@jax.jit
+def fwd_batch_opt_core(
+        input_embeddings, flat_items, flat_map, seq_lengths_batch):
+    flat_item_embeddings = input_embeddings[flat_items]
+    # Compute user embedding as mean of all purchased item embeddings.
+    user_embeddings = jax.ops.segment_sum(
+        flat_item_embeddings, flat_map, num_segments=_BATCH,
+        indices_are_sorted=True)
+    user_embeddings /= jnp.expand_dims(seq_lengths_batch, axis=-1)
+    logits = jnp.einsum('ij,kj->ki', input_embeddings, user_embeddings)
+    _, topk = jax.lax.top_k(logits, _K)
+    return topk
+
+
+# Not jittable due to dynamic repeat.
+def topk_batch_opt(
+        input_embeddings, seq_items_batch, seq_lengths_batch: jnp.ndarray):
+    batch_size = len(seq_items_batch)
+    # Flatten items from all examples into a single vector.
+    flat_items = jnp.concatenate(seq_items_batch, axis=0)
+    # Map back to example.
+    flat_map = jnp.repeat(jnp.arange(batch_size), seq_lengths_batch)
+    return fwd_batch_opt_core(
+        input_embeddings, flat_items, flat_map, seq_lengths_batch)
+
+
+def process_batch(
+        embeddings, seq_items_batch, seq_lengths_batch, cid_batch, predictions):
+    if len(cid_batch) == _BATCH:
+        topk_batch = topk_batch_opt(
+            embeddings, seq_items_batch, jnp.asarray(seq_lengths_batch))
+        topk_batch = topk_batch.tolist()
+        # for cid, topk_list in zip(cid_batch, topk_batch):
+        #    topk = [str(mapping[e]) for e in topk_list]
+        #    predictions.write(cid + ',' + ' '.join(topk) + '\n')
+    else:
+        for cid, history in zip(cid_batch, seq_items_batch):
+            topk_list = get_topk(embeddings[history], embeddings)
+            topk = [str(mapping[e.item()]) for e in topk_list]
+            predictions.write(cid + ',' + ' '.join(topk) + '\n')
 
 
 start = time.time()
@@ -41,12 +84,24 @@ pbar.set_description('KNN Search')
 item_freq = defaultdict(lambda: 0)
 with open(_DATASET + '\\predictions.csv', 'w') as predictions:
     predictions.write('customer_id,prediction\n')
+    seq_items_batch = []
+    seq_lengths_batch = []
+    cid_batch = []
     for index, cid in enumerate(pbar):
         history_length = min(seq_lengths[index], _HISTORY)
-        item_history = items[index][:-history_length]
-        topk = get_topk(embeddings[item_history], embeddings)
-        topk = [str(mapping[e.item()]) for e in topk]
-        predictions.write(cid + ',' + ' '.join(topk) + '\n')
+        item_history = items[index][-history_length:]
+        seq_items_batch.append(item_history)
+        seq_lengths_batch.append(history_length)
+        cid_batch.append(cid)
+        if len(seq_items_batch) == _BATCH:
+            process_batch(embeddings, seq_items_batch,
+                          seq_lengths_batch, cid_batch, predictions)
+            seq_lengths_batch = []
+            seq_items_batch = []
+            cid_batch = []
+    if len(cid_batch) > 0:
+        process_batch(embeddings, seq_items_batch,
+                      seq_lengths_batch, cid_batch, predictions)
     missing_customers = all_customers.difference(cid_map)
     global_top_k = list(item_freq.items())
     global_top_k.sort(key=lambda e: e[1], reverse=True)
