dd82f875f9fde3d8eeb630aad92f6a89772fb5cc
Amitabha Roy <amitabha.roy@gmail.com>
2023-09-08 23:45:58 -0300

    Create hm_post2.py
    Retrieve larger number of candidates and then rank by past frequency of purchase.

---
+++ hm/hm_post2.py
@@ -0,0 +1,143 @@
+import argparse
+from collections import defaultdict
+import pandas as pd
+import jax
+import jax.numpy as jnp
+from tqdm import tqdm
+import time
+
+_DATASET = 'C:\\Users\\aroym\\Downloads\\hm_data'
+_EMBEDDINGS = _DATASET + '/embeddings_test.npz'
+_HISTORY = 64
+_RETRIEVE_K = 200
+_K = 12
+_BATCH = 4096
+
+
+@jax.jit
+def topk_batch_opt(user_embeddings, item_embeddings):
+    logits = jnp.einsum('ij,kj->ki', item_embeddings, user_embeddings)
+    _, topk = jax.lax.top_k(logits, _RETRIEVE_K)
+    return topk
+
+
+def metrics(pred, truth):
+    truth_set = set(truth)
+    ap_list = []
+    hits = 0
+    for i in range(len(pred)):
+        if pred[i] in truth_set:
+            hits += 1
+            ap_list.append(hits/(1 + i))
+        else:
+            ap_list.append(0.0)
+    precision = hits/len(pred)
+    ap = sum(ap_list)/min(len(truth), len(pred))
+    return precision, ap
+
+
+def process_batch(
+    user_embeddings, item_embeddings, seq_items_batch, freq_batch,
+        cid_batch, predictions):
+    precisions = []
+    aps = []
+    topk_batch = topk_batch_opt(user_embeddings, item_embeddings)
+    topk_batch = topk_batch.tolist()
+    for cid, topk_list, past, freq in zip(
+            cid_batch, topk_batch, seq_items_batch, freq_batch):
+        topk_list = [(freq[index], index) for index in topk_list]
+        topk_list = [e[1] for e in sorted(topk_list)][:_K]
+        precision, ap = metrics(topk_list, past)
+        precisions.append(precision)
+        aps.append(ap)
+        topk = ['0' + str(mapping[e]) for e in topk_list]
+        predictions.write(cid + ',' + ' '.join(topk) + '\n')
+    return sum(precisions)/len(precisions), sum(aps)/len(aps)
+
+
+start = time.time()
+data = jnp.load(_DATASET + '/tensors_history.npz')
+items_dup = data['items']
+seq_lengths_dup = data['seq_lengths']
+items = data['items_dedup']
+seq_lengths = data['seq_length_dedup']
+item_embeddings = jnp.load(_EMBEDDINGS)['item_embeddings']
+user_embeddings = jnp.load(_EMBEDDINGS)['user_embeddings']
+mapping = pd.read_csv(_DATASET + '/item_map.csv')
+cid_map = pd.read_csv(_DATASET + '/cid_map.csv')
+cid_map = cid_map['customer_id'].to_list()
+all_customers = set(pd.read_csv(
+    _DATASET + '/customers.csv')['customer_id'].to_list())
+print(f'Loaded from disk in {time.time() - start} secs.')
+mapping.set_index('enum', inplace=True)
+mapping = mapping['article_id'].to_dict()
+print(user_embeddings.shape)
+print(item_embeddings.shape)
+print(items.shape)
+print(len(mapping.keys()))
+pbar = tqdm(cid_map)
+pbar.set_description('KNN Search')
+item_freq = defaultdict(lambda: 0)
+sum_precision = None
+sum_ap = None
+count_precision = 0
+with open(_DATASET + '/predictions.csv', 'w') as predictions:
+    predictions.write('customer_id,prediction\n')
+    seq_items_batch = []
+    freq_batch = []
+    cid_batch = []
+    for index, cid in enumerate(pbar):
+        if len(seq_items_batch) == _BATCH:
+            precision, ap = process_batch(
+                user_embeddings[index-_BATCH:index],
+                item_embeddings,
+                seq_items_batch,
+                freq_batch,
+                cid_batch, predictions)
+            if sum_precision is None:
+                sum_precision = precision
+                sum_ap = ap
+            else:
+                sum_precision += precision
+                sum_ap += ap
+            count_precision += 1
+            pbar.set_description(
+                f'KNN Search precision={sum_precision/count_precision:.4f} '
+                f'map = {sum_ap/count_precision:.4f}')
+            seq_items_batch = []
+            freq_batch = []
+            cid_batch = []
+        item_history = items[index][:seq_lengths[index]]
+        item_history = item_history[-_HISTORY:]
+        for i in item_history:
+            item_freq[i] += 1
+        example_freq = defaultdict(lambda: 0)
+        for i in items_dup[index][:seq_lengths_dup[index]]:
+            example_freq[i] += 1
+        seq_items_batch.append(item_history)
+        freq_batch.append(example_freq)
+        cid_batch.append(cid)
+
+    if len(cid_batch) > 0:
+        precision, ap = process_batch(
+            user_embeddings[-len(cid_batch):],
+            item_embeddings, seq_items_batch, freq_batch, cid_batch,
+            predictions)
+        if sum_precision is None:
+            sum_precision = precision
+            sum_ap = ap
+        else:
+            sum_precision += precision
+            sum_ap += ap
+        count_precision += 1
+    print(f'Avg Precision = {sum_precision/count_precision:.4f} '
+          f'map = {sum_ap/count_precision:.4f}')
+    missing_customers = all_customers.difference(cid_map)
+    global_top_k = list(item_freq.items())
+    global_top_k.sort(key=lambda e: e[1], reverse=True)
+    global_top_k = global_top_k[:_K]
+    global_top_k = ['0' + str(mapping[e[0]]) for e in global_top_k]
+    pbar = tqdm(missing_customers)
+    pbar.set_description('Missing customers')
+    for cid in pbar:
+        predictions.write(cid + ',' + ' '.join(global_top_k) + '\n')
