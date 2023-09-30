52289dc04ad4cd00257c77456ebe32904b363cf0
Amitabha Roy <amitabha.roy@gmail.com>
2023-09-30 14:01:08 -0300

    Update hm_embeddings.py
    Add neural network to transform item features.

--- hm/hm_embeddings.py
+++ hm/hm_embeddings.py
@@ -14,14 +14,82 @@ _BATCH = 128
 _LR = 1e-4
 _LAMBDA = 5e-1
 
+_ITEM_NET_HIDDEN_DIM = 32
+
 
 class ModelParameters(NamedTuple):
+    '''Holds all trainable weights.'''
     user_embeddings: jnp.ndarray
     user_age_vector: jnp.ndarray
     item_embeddings: jnp.ndarray
     color_group_embeddings: jnp.ndarray
     section_name_embeddings: jnp.ndarray
     garment_group_embeddings: jnp.ndarray
+    item_net_input_layer: jnp.ndarray
+    item_net_hidden_layer: jnp.ndarray
+    item_net_output_layer: jnp.ndarray
+
+    @classmethod
+    def factory(cls,
+                rng_key,
+                n_users,
+                n_articles,
+                n_color_groups,
+                n_section_names,
+                n_garment_groups):
+        '''Constructs and returns initialized model parameters'''
+        return ModelParameters(
+            user_embeddings=jax.random.normal(
+                rng_key, shape=(n_users, _DIM)) / 1000,
+            user_age_vector=jax.random.normal(rng_key + 1, shape=(_DIM,)) / 100,
+            item_embeddings=jax.random.normal(
+                rng_key + 2, shape=(n_articles, _DIM)) / 1000,
+            color_group_embeddings=jax.random.normal(
+                rng_key + 3, shape=(n_color_groups, _DIM)) / 100,
+            section_name_embeddings=jax.random.normal(
+                rng_key + 4, shape=(n_section_names, _DIM)) / 100,
+            garment_group_embeddings=jax.random.normal(
+                rng_key + 4, shape=(n_garment_groups, _DIM)) / 100,
+            item_net_input_layer=jax.random.normal(
+                rng_key + 5, shape=(_ITEM_NET_HIDDEN_DIM, _DIM))/_DIM,
+            item_net_hidden_layer=jax.random.normal(
+                rng_key + 6,
+                shape=(_ITEM_NET_HIDDEN_DIM, _ITEM_NET_HIDDEN_DIM))/_ITEM_NET_HIDDEN_DIM,
+            item_net_output_layer=jax.random.normal(
+                rng_key + 7,
+                shape=(_DIM, _ITEM_NET_HIDDEN_DIM))/_ITEM_NET_HIDDEN_DIM,
+        )
+
+    def user_embedding_vectors(self,
+                               batch_user_indices,
+                               batch_user_ages):
+        '''Computes the user embedding vectors.'''
+        features = (self.user_embeddings[batch_user_indices] +
+                    jnp.expand_dims(batch_user_ages, axis=1) *
+                    jnp.expand_dims(self.user_age_vector, axis=0))
+        return features
+
+    def item_embedding_vectors(self,
+                               batch_articles_color_group,
+                               batch_articles_section_name,
+                               batch_articles_garment_group):
+        '''Computes the item embedding vectors.'''
+        features = (self.color_group_embeddings[batch_articles_color_group] +
+                    self.section_name_embeddings[batch_articles_section_name] +
+                    self.garment_group_embeddings[batch_articles_garment_group]
+                    )
+        transformed_features = jnp.einsum(
+            'bf,hf->bh', features, self.item_net_input_layer)
+        transformed_features = jax.nn.relu(transformed_features)
+        transformed_features = jnp.einsum(
+            'bi,ij->bj', transformed_features, self.item_net_hidden_layer)
+        transformed_features = jax.nn.relu(transformed_features)
+        transformed_features = jnp.einsum(
+            'bi,io->bo', transformed_features, self.item_net_output_layer)
+        return (
+            self.item_embeddings +
+            transformed_features
+        )
 
 
 parser = argparse.ArgumentParser()
@@ -54,25 +122,13 @@ key = jax.random.PRNGKey(42)
 item_embeddings = None
 if args.start_epoch == -1:
     print(f'Initializing parameters for {n_articles} items {n_users} users')
-
-    user_embeddings = jax.random.normal(key, shape=(n_users, _DIM))/1000
-    user_age_vector = jax.random.normal(key + 1, shape=(_DIM,))/100
-
-    item_embeddings = jax.random.normal(key + 2, shape=(n_articles, _DIM))/1000
-    color_group_embeddings = jax.random.normal(
-        key + 3, shape=(n_color_groups, _DIM))/100
-    section_name_embeddings = jax.random.normal(
-        key + 4, shape=(n_section_names, _DIM))/100
-    garment_group_embeddings = jax.random.normal(
-        key + 4, shape=(n_garment_groups, _DIM))/100
-    model_parameters = ModelParameters(
-        user_embeddings=user_embeddings,
-        user_age_vector=user_age_vector,
-        item_embeddings=item_embeddings,
-        color_group_embeddings=color_group_embeddings,
-        section_name_embeddings=section_name_embeddings,
-        garment_group_embeddings=garment_group_embeddings,
-    )
+    model_parameters = ModelParameters.factory(
+        rng_key=key,
+        n_users=n_users,
+        n_articles=n_articles,
+        n_color_groups=n_color_groups,
+        n_section_names=n_section_names,
+        n_garment_groups=n_garment_groups)
 else:
     print(f'Loading embeddings from checkpoint.')
     checkpoint = jnp.load(_DATASET + f'/embeddings_{args.start_epoch}.npz')
@@ -86,30 +142,22 @@ for name, value in model_parameters._asdict().items():
 
 
 @jax.jit
-def fwd_batch_opt_core(input_item_embeddings,
-                       input_color_group_embeddings,
-                       input_section_name_embeddings,
-                       input_garment_group_embeddings,
+def fwd_batch_opt_core(model_params,
                        articles_color_group,
                        articles_section_name,
                        articles_garment_group,
-                       input_user_embeddings,
-                       input_customer_age_vector,
+                       user_indices_batch,
                        customer_ages_batch,
                        flat_items,
                        flat_map):
     # Modify user embeddings by input features.
-    input_user_embeddings = (
-        input_user_embeddings +
-        jnp.expand_dims(customer_ages_batch, axis=1) *
-        jnp.expand_dims(input_customer_age_vector, axis=0)
-    )
+    input_user_embeddings = model_params.user_embedding_vectors(
+        user_indices_batch, customer_ages_batch)
     # Modify item embeddings by input features.
-    input_item_embeddings = (
-        input_item_embeddings +
-        input_color_group_embeddings[articles_color_group] +
-        input_section_name_embeddings[articles_section_name] +
-        input_garment_group_embeddings[articles_garment_group]
+    input_item_embeddings = model_params.item_embedding_vectors(
+        batch_articles_color_group=articles_color_group,
+        batch_articles_section_name=articles_section_name,
+        batch_articles_garment_group=articles_garment_group
     )
     # Note: negation in next line is reversed for positive examples
     # in the following line to it.
@@ -130,35 +178,20 @@ def fwd_batch_opt(params: ModelParameters,
                   seq_items_batch,
                   seq_lengths_batch: jnp.ndarray,
                   user_indices_batch: jnp.ndarray):
-    (input_item_embeddings,
-     input_color_group_embeddings,
-     input_section_name_embeddings,
-     input_garment_group_embeddings,
-     input_user_embeddings,
-     input_customer_age_vector,
-     ) = (
-        params.item_embeddings,
-        params.color_group_embeddings,
-        params.section_name_embeddings,
-        params.garment_group_embeddings,
-        params.user_embeddings,
-        params.user_age_vector,
-    )
     batch_size = len(seq_items_batch)
     # Flatten items from all examples into a single vector.
     flat_items = jnp.concatenate(seq_items_batch, axis=0)
     # Map back to example.
     flat_map = jnp.repeat(jnp.arange(batch_size), seq_lengths_batch)
-    return fwd_batch_opt_core(input_item_embeddings,
-                              input_color_group_embeddings,
-                              input_section_name_embeddings,
-                              input_garment_group_embeddings,
+    return fwd_batch_opt_core(params,
+                              # Item features.
                               articles_color_group,
                               articles_section_name,
                               articles_garment_group,
-                              input_user_embeddings[user_indices_batch],
-                              input_customer_age_vector,
+                              # User features.
+                              user_indices_batch,
                               customer_ages_batch,
+                              # Transactions.
                               flat_items, flat_map)
 
 
